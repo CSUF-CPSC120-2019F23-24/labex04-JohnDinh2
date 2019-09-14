468a122a3da248a164d2d9d2c363f1cc6d146a20
johndinh391 <johndinh391@cs104u-2c.acad.fullerton.edu>
2019-09-14 14:37:59 -0700

    TODO's done.

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -8,7 +8,7 @@ int main()
   // then print both the char and the short
 
   char my_char1 = 'J';
-  short my_short1 = ;
+  short my_short1 = static_cast<short>(my_char1);
   std::cout << "The ASCII value for " << my_char1 << " is " << my_short1 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -16,7 +16,7 @@ int main()
   // TODO#2: add one to the character to see what gets printed out
 
   char my_char2 = 'C';
-  my_char2 = ;
+  my_char2 = my_char2 + 1;
   std::cout << "Char1: " << my_char2 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -26,9 +26,9 @@ int main()
   // longer alphabetic
 
   char my_char3 = 'z';
-  my_char3 = ;
-  std::cout << "Char2: " << my_char3 << std::endl;
-
+  my_char3 = my_char3 + 1;
+  std::cout << "Char2: " << my_char3 << std::endl;/* The value is no longer alphabetic because there is no other letter after z,
+  which would mean there is nothing in the alphabet to represent. */
   std::cout << std::endl; // blank line to separate output from the different exercises
 
   // TODO#4: print each number, then add one to each number and print it again
@@ -39,16 +39,15 @@ int main()
 
   short my_short2 = 32767; // maximum value for a short
   int my_int2 = my_short2;
-
   std::cout << "Short (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_short2 << std::hex << " (0x" << my_short2 << ')';
-  my_short2 = ;
-  std::cout << ", after adding 1: " << std::dec << my_short2
+  my_short2 = my_short2 + 1;
+  std::cout << ", after adding 1: " << std::dec << my_short2 //my_short2 became negative because it was past the maximum value for a short, and so overflowed.
     << std::hex << " (0x" << my_short2 << ")\n";
 
   std::cout << "Int (" << sizeof(my_short2) << " bytes): "
-    << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';
-  my_int2 = ;
+    << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';//my_int2 did not go past the max value for an int, so it output the correct number.
+  my_int2 = my_int2 + 1;
   std::cout << ", after adding 1: " << std::dec << my_int2
     << std::hex << " (0x" << my_int2 << ")\n";
 
@@ -60,17 +59,16 @@ int main()
 
   unsigned short my_short3 = 65535; // maximum value for an unsigned short
   int my_int3 = my_short3;
-
   std::cout << "Unsigned short (" << sizeof(my_short3) << " bytes): "
     << std::dec << my_short3 << std::hex << " (0x" << my_short3 << ')';
-  my_short3 = ;
-  std::cout << ", after adding 1: " << std::dec << my_short3
+  my_short3 = my_short3 + 1;
+  std::cout << ", after adding 1: " << std::dec << my_short3//my_short3 shows 0 because it overflowed and so had to start from the lowest value possible for a short
     << std::hex << " (0x" << my_short3 << ")\n";
 
   std::cout << "Same value as int (" << sizeof(my_int3) << " bytes): "
     << std::dec << my_int3 << std::hex << " (0x" << my_int3 << ')';
-  my_int3 = ;
-  std::cout << ", after adding 1: " << std::dec << my_int3
+  my_int3 = my_int3 + 1;
+  std::cout << ", after adding 1: " << std::dec << my_int3//my_int3 has a far larger max value, and so portrays the proper integer after one was added.
     << std::hex << " (0x" << my_int3 << ")\n";
 
   // ** not required, but for more practice, feel free to add code below which **
