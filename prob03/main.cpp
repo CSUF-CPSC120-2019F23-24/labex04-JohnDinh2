08abe2b9f5f95ace49ab3a31edd686a6d232df80
johndinh391 <johndinh391@cs104u-2c.acad.fullerton.edu>
2019-09-14 14:35:39 -0700

    Program made.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // Sales prediction
+#include <iostream>
+#include <string>
+
+int main()
+{
+  const int sales_of_last_year = 2103419277;
+
+  double sales_percentage = 1.18;//calculate the sales estimated to be this year compared to the previous year
+
+  unsigned int sales_of_this_year = sales_of_last_year * sales_percentage;//multiply the percentage to last year's sales
+
+  std::cout << "Sales of last year were: $" << sales_of_last_year << std::endl;//Output the results for this year and last year.
+
+  std::cout << "Sales of this year were: $" << sales_of_this_year << std:: endl;
+}
